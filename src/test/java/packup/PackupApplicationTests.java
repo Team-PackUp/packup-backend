a5a919e3d9af478bf5b9cf1f00be210cc45cd1e9
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2025-04-07 17:42:25 +0900

    feat: 프로젝트 초기 설정

---
+++ src/test/java/packup/PackupApplicationTests.java
@@ -0,0 +1,13 @@
+package packup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
