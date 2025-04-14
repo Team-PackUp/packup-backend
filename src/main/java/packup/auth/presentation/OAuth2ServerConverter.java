bb399b393820279304a764af05d5fead146b2f5c
i3months <waysoleward01@naver.com>
2025-04-15 00:53:37 +0900

    feat: OAuth2 타입 컨버터 연결

--- src/main/java/packup/auth/presentation/OAuth2ServerConverter.java
+++ src/main/java/packup/auth/presentation/OAuth2ServerConverter.java
@@ -1,8 +1,10 @@
 package packup.auth.presentation;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 import packup.auth.domain.OAuth2ServerType;
 
+@Component
 public class OAuth2ServerConverter implements Converter<String, OAuth2ServerType> {
 
     @Override
