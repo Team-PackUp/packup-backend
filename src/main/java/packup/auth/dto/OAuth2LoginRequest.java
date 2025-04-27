112c7d0d13a235fda05369caf112a9abf4d12b30
jmj <waysoleward01@naver.com>
2025-04-27 20:58:35 +0900

    fix: 키 이름 일치

--- src/main/java/packup/auth/dto/OAuth2LoginRequest.java
+++ src/main/java/packup/auth/dto/OAuth2LoginRequest.java
@@ -1,8 +1,11 @@
 package packup.auth.dto;
 
 import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 public class OAuth2LoginRequest {
+
+    @JsonProperty("access_token")
     private String accessToken;
 }
