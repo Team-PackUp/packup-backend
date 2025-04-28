e44b1c45f7533cca867ef0fd2daaffad5471c0cf
jmj <waysoleward01@naver.com>
2025-04-28 15:33:01 +0900

    feat: 프론트와 ResponseType 맞춰주기 - ResultModel

---
+++ src/main/java/packup/common/enums/ResponseMessage.java
@@ -0,0 +1,26 @@
+package packup.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseMessage {
+    SUCCESS(OK, "요청에 성공했습니다."),
+    FAIL(BAD_REQUEST, "요청에 실패했습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+
+    public String message() {
+        return message;
+    }
+}
