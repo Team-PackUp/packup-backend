53e7c71fc8f53fc5571fd383074efb00705cd364
jmj <waysoleward01@naver.com>
2025-04-28 15:49:17 +0900

    feat: JSON -> Map으로 변환해서 반환. 공통 기능 JSONUtil 추가

--- src/main/java/packup/user/dto/UserInfoResponse.java
+++ src/main/java/packup/user/dto/UserInfoResponse.java
@@ -1,22 +1,33 @@
 package packup.user.dto;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.Builder;
 import lombok.Getter;
+import packup.common.util.JsonUtil;
 import packup.user.domain.UserInfo;
+
+import java.util.Map;
 
 @Getter
 @Builder
 public class UserInfoResponse {
     // 필요한거 여기서 추가..
-    String email;
-    String preferCategorySeqJson;
-    String nickname;
+    private String email;
+    private Map<String, Object> preferCategorySeqJson;
+    private String nickname;
 
     // 가입할 때 테이블 세 군데에 insert 해 주니까 null 체크 안함
+    // JSON으로 받아온 데이터를 Map으로 파싱한 후 반환해 줘야 함
     public static UserInfoResponse of(UserInfo userInfo) {
+
+        Map<String, Object> preferCategory = JsonUtil.fromJson(
+                userInfo.getPrefer().getPreferCategorySeqJson(),
+                new TypeReference<Map<String, Object>>() {}
+        );
+
         return UserInfoResponse.builder()
                 .email(userInfo.getEmail())
-                .preferCategorySeqJson(userInfo.getPrefer().getPreferCategorySeqJson())
+                .preferCategorySeqJson(preferCategory)
                 .nickname(userInfo.getDetailInfo().getNickname())
                 .build();
     }
