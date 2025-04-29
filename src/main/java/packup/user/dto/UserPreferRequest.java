c124a0a1c3c36035d415d4b33e061e84988ae58d
jmj <waysoleward01@naver.com>
2025-04-30 00:41:44 +0900

    feat: 사용자 선호도 api 개발

---
+++ src/main/java/packup/user/dto/UserPreferRequest.java
@@ -0,0 +1,10 @@
+package packup.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserPreferRequest {
+    private String preferCategorySeqJson;
+}
