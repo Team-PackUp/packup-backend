9d119cc07c88afa7c8eb9b1e86563af124cd60ad
jmj <waysoleward01@naver.com>
2025-05-07 00:00:08 +0900

    feat: payment confirm 관련 로직 작성

--- src/main/java/packup/payment/presentation/PaymentApiController.java
+++ src/main/java/packup/payment/presentation/PaymentApiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import packup.auth.annotation.Auth;
 import packup.common.dto.ResultModel;
 import packup.payment.dto.PaymentConfirmRequest;
 import packup.payment.dto.PaymentConfirmResponse;
@@ -18,8 +19,9 @@ public class PaymentApiController {
     private final PaymentService paymentService;
 
     @PostMapping("/confirm")
-    public ResultModel<PaymentConfirmResponse> confirmPayment(@RequestBody PaymentConfirmRequest request) {
-        return null;
+    public ResultModel<PaymentConfirmResponse> confirmPayment(@Auth Long memberId, @RequestBody PaymentConfirmRequest request) {
+        PaymentConfirmResponse paymentConfirmResponse = paymentService.confirm(memberId, request);
+        return ResultModel.success(paymentConfirmResponse);
     }
 
 }
