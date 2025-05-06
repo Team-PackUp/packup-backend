1291411cb82f5b54c223a0cdaf5439d7b872df31
jmj <waysoleward01@naver.com>
2025-05-06 23:30:26 +0900

    feat: 결제 이력 관리 엔티티 설정

---
+++ src/main/java/packup/payment/domain/PaymentReceipt.java
@@ -0,0 +1,25 @@
+package packup.payment.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import packup.common.domain.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "payment_receipt")
+public class PaymentReceipt extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_id", nullable = false)
+    private PaymentHistory payment;
+
+    @Lob
+    private String receiptUrl;
+
+    public void assignPayment(PaymentHistory payment) {
+        this.payment = payment;
+    }
+}
