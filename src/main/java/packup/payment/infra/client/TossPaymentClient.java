c491cc74898b1c11375c882497e057d9ff3930ab
jmj <waysoleward01@naver.com>
2025-05-06 21:11:56 +0900

    feat: 시크릿 키 헤더 설정

--- src/main/java/packup/payment/infra/client/TossPaymentClient.java
+++ src/main/java/packup/payment/infra/client/TossPaymentClient.java
@@ -1,6 +1,7 @@
 package packup.payment.infra.client;
 
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.service.annotation.PostExchange;
 import packup.payment.dto.PaymentConfirmRequest;
 import packup.payment.dto.PaymentConfirmResponse;
@@ -8,5 +9,8 @@ import packup.payment.dto.PaymentConfirmResponse;
 public interface TossPaymentClient {
 
     @PostExchange(url = "https://api.tosspayments.com/v1/payments/confirm")
-    PaymentConfirmResponse confirmPayment(@RequestBody PaymentConfirmRequest request);
+    PaymentConfirmResponse confirmPayment(
+            @RequestBody PaymentConfirmRequest request,
+            @RequestHeader("Authorization") String authorization
+    );
 }
