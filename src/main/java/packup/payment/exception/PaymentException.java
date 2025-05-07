0b7b112e168d82bf0aa7e14c99efbf8dc6de62b2
jmj <waysoleward01@naver.com>
2025-05-07 09:15:22 +0900

    feat: payment 전용 예외처리 진행

---
+++ src/main/java/packup/payment/exception/PaymentException.java
@@ -0,0 +1,17 @@
+package packup.payment.exception;
+
+import lombok.RequiredArgsConstructor;
+import packup.auth.exception.AuthExceptionType;
+import packup.common.exception.BaseException;
+import packup.common.exception.BaseExceptionType;
+
+@RequiredArgsConstructor
+public class PaymentException extends BaseException {
+
+    private final AuthExceptionType exceptionType;
+
+    @Override
+    public BaseExceptionType exceptionType() {
+        return exceptionType;
+    }
+}
