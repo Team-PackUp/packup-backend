8337c4732e444295764125a9194038efd97f4488
butwhole1994 <ato1994@naver.com>
2025-05-12 18:04:28 +0900

    feat: tour 도메인 작업중

--- src/main/java/packup/tour/repositoroy/TourInfoRepository.java
+++ src/main/java/packup/tour/repositoroy/TourInfoRepository.java
@@ -1,11 +1,8 @@
 package packup.tour.repositoroy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import packup.tour.domain.TourInfo;
 
-import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -26,13 +23,4 @@ public interface TourInfoRepository extends JpaRepository<TourInfo, Long> {
      * @return 해당 가이드의 투어 리스트
      */
     List<TourInfo> findByGuideSeq(Long guideSeq);
-
-    /**
-     * 현재 날짜 기준으로 모집 기간 내에 있는 투어를 조회.
-     *
-     * @param today 기준 날짜 (보통 LocalDate.now())
-     * @return 현재 모집 중인 투어 리스트
-     */
-    @Query("SELECT t FROM tour_info t WHERE :today BETWEEN t.recruitmentPeriod.startDate AND t.recruitmentPeriod.endDate")
-    List<TourInfo> findRecruitingTours(@Param("today") LocalDate today);
 }
