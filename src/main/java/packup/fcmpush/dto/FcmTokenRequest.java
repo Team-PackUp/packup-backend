d5bbc58c158914eb49cd372dea7e796f6af6276d
jmj <waysoleward01@naver.com>
2025-05-16 13:56:19 +0900

    refactor: FcmPush 기능 구현

---
+++ src/main/java/packup/fcmpush/dto/FcmTokenRequest.java
@@ -0,0 +1,12 @@
+package packup.fcmpush.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FcmTokenRequest {
+    private String fcmToken;
+}
---
+++ src/main/java/packup/fcmpush/presentation/FcmPushController.java
@@ -0,0 +1,29 @@
+package packup.fcmpush.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import packup.auth.annotation.Auth;
+import packup.common.dto.ResultModel;
+import packup.fcmpush.dto.FcmTokenRequest;
+import packup.fcmpush.service.FcmPushService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notification")
+public class FcmPushController {
+
+    private final FcmPushService notificationService;
+
+    @PostMapping("/register")
+    public ResultModel<Void> registerFcmToken(
+            @Auth Long memberId,
+            @RequestBody FcmTokenRequest request
+    ) {
+        notificationService.registerOrUpdateFcmToken(memberId, request.getFcmToken());
+        return ResultModel.success();
+    }
+
+}
