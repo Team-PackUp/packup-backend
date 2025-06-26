75dd90b7e43762aeb3f3deaab1e8aaae27b09a43
Juna Im <dlawnsdk04@naver.com>
2025-06-26 21:02:17 +0900

    feat: 추천 상품 API response

--- src/main/java/packup/recommend/dto/RecommendResponse.java
+++ src/main/java/packup/recommend/dto/RecommendResponse.java
@@ -3,12 +3,10 @@ package packup.recommend.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import packup.tour.dto.TourInfoResponse;
 
 @Getter
 @Builder
 public class RecommendResponse {
-    private final Long userSeq;
-    private Long tourSeq;
-    private String actionType;
-    private final float score;
+    private final TourInfoResponse tour;
 }
