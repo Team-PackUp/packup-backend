82b61763c972b74834aae2035ed02a1f1417cde9
jmj <waysoleward01@naver.com>
2025-09-07 19:20:17 +0900

    feat: activity thumbnail 리포지토리 메서드 추가

--- src/main/java/packup/tour/domain/repository/TourActivityThumbnailRepository.java
+++ src/main/java/packup/tour/domain/repository/TourActivityThumbnailRepository.java
@@ -3,5 +3,9 @@ package packup.tour.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import packup.tour.domain.TourActivityThumbnail;
 
+import java.util.List;
+
 public interface TourActivityThumbnailRepository extends JpaRepository<TourActivityThumbnail, Long> {
+    void deleteByTourActivitySeq(Long activitySeq);
+    List<TourActivityThumbnail> findAllByTourActivitySeqOrderByThumbnailOrderAsc(Long activitySeq);
 }
