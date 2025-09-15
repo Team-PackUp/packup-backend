8fcebea182003771a1dae3ed4d2f81718901aa06
jmj <waysoleward01@naver.com>
2025-09-15 12:46:17 +0900

    feat: componentscan 추가

--- src/main/java/packup/PackUpApplication.java
+++ src/main/java/packup/PackUpApplication.java
@@ -2,10 +2,12 @@ package packup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "packup")
 public class PackUpApplication {
 
 	public static void main(String[] args) {
